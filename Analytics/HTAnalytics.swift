a8e57e36ef21dcd35c98ef0fbd3d9a3e7242511b
Himadri Jyoti <himadrisj@gmail.com>
2018-02-01 12:13:39 +0530

    Integrate Aspects method swizzling library and fix warnings in the same library

---
+++ Analytics/HTAnalytics.swift
@@ -0,0 +1,26 @@
+//
+//  HTAnalytics.swift
+//  Analytics
+//
+//  Created by Test on 01/02/18.
+//  Copyright © 2018 Analytics. All rights reserved.
+//
+
+import Foundation
+import HTAnalyticsPrivate
+
+public enum TrackingType: Int {
+    case buttonClick = 1
+    case viewLoad = 2
+}
+
+public final class HTAnalytics {
+    
+    public func startTracking(trackingDetails: Int) {
+        
+    }
+    
+    public func stopTracking(trackingDetails: Int) {
+        
+    }
+}
